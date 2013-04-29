0c0c9e9a48d43597ef7c5dc64658479ad4936ae5
Andrey Shchekin <ashmind@gmail.com>
2013-04-30 00:20:56 +1200

    Added nuspec generation to build and removed unnecessary dependencies.

--- IntegrationTests/Internal/TestHostStartup.cs
+++ IntegrationTests/Internal/TestHostStartup.cs
@@ -4,7 +4,7 @@ using Owin;
 namespace Gate.Adapters.AspNet.IntegrationTests.Internal {
     public class TestHostStartup {
         public void Configuration(IAppBuilder app) {
-            var webSitePath = Path.Combine(Path.GetDirectoryName(this.GetType().Assembly.Location), @"..\..\..\IntegrationTests.WebSite");
+            var webSitePath = Path.Combine(Path.GetDirectoryName(this.GetType().Assembly.Location), @"..\..\..\TestWebSite");
             webSitePath = Path.GetFullPath(webSitePath);
             app.Use(typeof(AspNetAdapter), webSitePath);
         }
