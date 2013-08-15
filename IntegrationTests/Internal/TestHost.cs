7e8cb51182b7561fc49c9524feba08a853efca54
Nicolai Willems <niwil90@gmail.com>
2013-08-15 16:51:19 +0200

    Test were broken
    Main problem was some assembly referencing - this could be due to changes in xUnit.

--- IntegrationTests/Internal/TestHost.cs
+++ IntegrationTests/Internal/TestHost.cs
@@ -6,7 +6,7 @@ namespace Gate.Adapters.AspNet.IntegrationTests.Internal {
         public const string TestApplicationDataValue = "Magic";
         public const string TestApplicationDataProviderValue = "Mirror";
 
-        private static readonly Uri _url = new Uri("http://localhost:8087");
+        private static readonly Uri _url = new Uri("http://localhost:60880");
 
         // causes static constructor to run
         public static Uri Url {
--- IntegrationTests/Internal/TestHostStartup.cs
+++ IntegrationTests/Internal/TestHostStartup.cs
@@ -5,7 +5,9 @@ using Owin;
 namespace Gate.Adapters.AspNet.IntegrationTests.Internal {
     public class TestHostStartup {
         public void Configuration(IAppBuilder app) {
-            var webSitePath = Path.Combine(Path.GetDirectoryName(this.GetType().Assembly.Location), @"..\..\..\TestWebSite");
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            //CodeBase references where the assembly originally was stored. Location pointed to somewhere Temp
+            var webSitePath = Path.Combine(Path.GetDirectoryName(assembly.CodeBase), @"..\..\..\TestWebSite");
             webSitePath = Path.GetFullPath(webSitePath);
 
             var testApplicationData = new Dictionary<string, object> {
